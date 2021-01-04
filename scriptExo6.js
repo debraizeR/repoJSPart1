bdbd85d981d04fc858e9eb026f6a626ad1e4eb32
Raphael Debraize <raphaeldebraize@gmail.com>
2021-01-04 09:44:45 +0100

    Code JS de l'exercice 6

---
+++ scriptExo6.js
@@ -0,0 +1,5 @@
+function maFonction(){
+    let firstNumber = parseInt(document.getElementById("firstNumber").value);
+    let secondNumber = Number(document.getElementById("secondNumber").value);
+    window.alert("Le reste de " + firstNumber + " / " + secondNumber + " est " +(firstNumber % secondNumber));
+}  
